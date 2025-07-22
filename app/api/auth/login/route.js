66c1cdd151f60c097bbfbb225830547791d6b5a2
Sami <samikhan10902@gmail.com>
2025-07-22 23:07:38 +0530

    done1

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { connectDB } from "../../../../lib/mongodb";
 import User from "../../../../models/User";
